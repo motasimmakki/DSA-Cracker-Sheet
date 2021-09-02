92758570f718d22525012015772d9ff0a79afae6
Motasim <makkimotasim@gmail.com>
2021-09-02 17:03:18 +0530

    feat: Reverse the Array

---
+++ Array/reverse_the_array.cpp
@@ -0,0 +1,33 @@
+//Initial Template for C++
+#include<bits/stdc++.h>
+using namespace std;
+
+string reverseWord(string str);
+
+int main() {	
+	int t;
+	cin>>t;
+	while(t--){
+        string s;
+        cin >> s;
+        
+        cout << reverseWord(s) << endl;
+	}
+
+	return 0;
+}
+// } Driver Code Ends
+
+//User function Template for C++
+void swap(char &ch1, char &ch2){
+    char temp = ch1;
+    ch1 = ch2;
+    ch2 = temp;
+}
+
+string reverseWord(string str){
+  for(int i = 0; i  < str.length()/2; i++){
+      swap(str[i], str[str.length()-i-1]);
+  }
+  return str;
+}
