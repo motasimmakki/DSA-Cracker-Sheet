bb82b676518985c052f4e24ee361bf5694d896a3
Motasim <makkimotasim@gmail.com>
2021-09-02 21:25:12 +0530

    build: Added driver code

--- Array/min_max_in_minimum_operations.cpp
+++ Array/min_max_in_minimum_operations.cpp
@@ -6,7 +6,19 @@ typedef struct Pair{
     int max;
 }MinMax;
 
+MinMax getMinMax(int arr[], int n){
+    MinMax pair{0, 0};
+
+    return pair;
+}
+
 int main(){
-    
+    int arr[] = {1000, 11, 445, 1, 330, 3000};
+    int size = sizeof(arr)/sizeof(arr[0]);
+
+    MinMax pair = getMinMax(arr, size);
+    cout<< "\nMinimum In An Array Is: "<< pair.min<< endl;
+    cout<< "\nMaximum In An Array Is: "<< pair.max<< endl;
+     
     return 0;
 }
