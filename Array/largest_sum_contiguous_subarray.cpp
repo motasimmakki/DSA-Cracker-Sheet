b857e2b4ebb557d423dc35e37f9ab172d04eb07a
Motasim <makkimotasim@gmail.com>
2021-09-05 14:36:02 +0530

    feat: Module getMaxSubArraySum

--- Array/largest_sum_contiguous_subarray.cpp
+++ Array/largest_sum_contiguous_subarray.cpp
@@ -15,12 +15,22 @@
 using namespace std;
 
 int getMaxSubarraySum(int arr[], int n){
-
+    int currSum = arr[0];
+    int bestSum = arr[0];
+    for(int i = 1; i < n; i++){
+        if(currSum >= 0)
+            currSum += arr[i];
+        else
+            currSum = arr[i];
+        if(bestSum < currSum)
+            bestSum = currSum;
+    }
+    return bestSum;
 }
 
 int main(){
-    // int arr[] = {1,2,3,-2,5};
-    int arr[] = {-1,-2,-3,-4};
+    int arr[] = {1,2,3,-2,5};
+    // int arr[] = {-1,-2,-3,-4};
     int size = sizeof(arr)/sizeof(arr[0]);
 
     cout<< "\nMaximum Sub-Array Sum Is: "<< getMaxSubarraySum(arr, size)<< endl;
