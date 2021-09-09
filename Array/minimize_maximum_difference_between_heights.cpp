297bc9b97b5ef5aaeb01b4d7bcbbcfad9ea837e3
Motasim <makkimotasim@gmail.com>
2021-09-09 15:18:23 +0530

    feat: Module getMinDiff()

--- Array/minimize_maximum_difference_between_heights.cpp
+++ Array/minimize_maximum_difference_between_heights.cpp
@@ -15,15 +15,27 @@
 using namespace std;
 
 int getMinDiff(int arr[], int n, int k){
-    int result;
+    sort(arr, arr+n);
+    
+    int result = arr[n-1]-arr[0];
+    int smallest = arr[0]+k;
+    int largest = arr[n-1]-k;
 
+    int minimum, maximum;
+    for(int i = 0; i < n-1; i++){
+        minimum = min(smallest, arr[i+1]-k);
+        maximum = max(largest, arr[i]+k);
+        if(minimum < 0)
+            continue;
+        result = min(result, maximum-minimum);
+    }
 
     return result;
 }
 
 int main(){
-    // int arr[] = {1, 5, 8, 10}, k = 2;
-    int arr[] = {3, 9, 12, 16, 20}, k = 3;
+    int arr[] = {1, 5, 8, 10}, k = 2;
+    // int arr[] = {3, 9, 12, 16, 20}, k = 3;
     int size = sizeof(arr)/sizeof(arr[0]);
 
     cout<< "\nMinimum Difference After Operation Is: "<< getMinDiff(arr, size, k);
