ae0be0ad5ad496a7a055cf6cefbcde1c303a00b9
Motasim <makkimotasim@gmail.com>
2021-09-10 20:55:39 +0530

    feat: mergeTwoSortedArr()

--- Array/merge_two_sorted_arrays.cpp
+++ Array/merge_two_sorted_arrays.cpp
@@ -1,8 +1,16 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 
 void mergeTwoSortedArr(int arr1[], int n, int arr2[], int m){
-
+    int i = n-1, j = 0;
+    while((i >= 0) and (j < m)){
+        if(arr1[i] > arr2[j])
+            swap(arr1[i], arr2[j]);
+        i--; j++;
+    }
+    sort(arr1, arr1+n);
+    sort(arr2, arr2+n);
 }
 
 void printArr(int arr[], int n){
@@ -12,10 +20,10 @@ void printArr(int arr[], int n){
 }
 
 int main(){
-    int arr1[] = {};
-    int size1 = sizeof(arr1)/sizeof(arr1);
-    int arr2[] = {};
-    int size2 = sizeof(arr2)/sizeof(arr2);
+    int arr1[] = {1, 3, 5, 7};
+    int size1 = sizeof(arr1)/sizeof(arr1[0]);
+    int arr2[] = {0, 2, 6, 8, 9};
+    int size2 = sizeof(arr2)/sizeof(arr2[0]);
 
     cout<< "\nArray Before Merging: "<< endl;
     printArr(arr1, size1); printArr(arr2, size2);
